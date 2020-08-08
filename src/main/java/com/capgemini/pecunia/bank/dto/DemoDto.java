7c132cbe9d0d85495b5e932a45482dd90eb4db63
lovaraju <lovaraju@lovaraju-Inspiron-15-3567>
2020-08-08 19:16:44 +0530

    dto

--- src/main/java/com/capgemini/pecunia/bank/dto/DemoDto.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.pecunia.bank.dto;
-
-public class DemoDto {
-
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/main/java/com/capgemini/pecunia/bank/dto/TxnForm.java
@@ -0,0 +1,22 @@
+package com.capgemini.pecunia.bank.dto;
+
+public class TxnForm {
+
+	private String accId;
+	private double amount;
+	
+	public String getAccId() {
+		return accId;
+	}
+	public void setAccId(String accId) {
+		this.accId = accId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+}
