e5eb7db29c34e7d4b4156f8a7e459bfd51cb897b
lovaraju <lovaraju@lovaraju-Inspiron-15-3567>
2020-08-08 19:19:07 +0530

    Exceptions Added

---
+++ src/main/java/com/capgemini/pecunia/bank/exceptions/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.capgemini.pecunia.bank.exceptions;
+
+public class AccountNotFoundException extends Exception{
+
+	public AccountNotFoundException(String message) {
+		super(message);
+		
+	}
+}
---
+++ src/main/java/com/capgemini/pecunia/bank/exceptions/TransactionException.java
@@ -0,0 +1,9 @@
+package com.capgemini.pecunia.bank.exceptions;
+
+public class TransactionException extends Exception {
+
+	public TransactionException(String message) {
+		super(message);
+	}
+	 
+}
