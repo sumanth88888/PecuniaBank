b7fb75b707914733baa612d662890186cf8971d0
lovaraju <lovaraju@lovaraju-Inspiron-15-3567>
2020-08-09 19:57:03 +0530

    Dto Added

---
+++ src/main/java/com/capgemini/pecunia/bank/dto/BankMessage.java
@@ -0,0 +1,22 @@
+package com.capgemini.pecunia.bank.dto;
+
+public class BankMessage {
+	
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public BankMessage(String msg) {
+		super();
+		this.msg = msg;
+	}
+	
+	
+
+}
