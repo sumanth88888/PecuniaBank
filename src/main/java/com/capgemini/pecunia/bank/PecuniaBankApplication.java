12740b9c3dc5c23087f12c064642d9738c545a7e
sumanth88888 <56868016+sumanth88888@users.noreply.github.com>
2020-08-15 16:49:19 +0530

    Update PecuniaBankApplication.java

--- src/main/java/com/capgemini/pecunia/bank/PecuniaBankApplication.java
+++ src/main/java/com/capgemini/pecunia/bank/PecuniaBankApplication.java
@@ -1,7 +1,10 @@
 package com.capgemini.pecunia.bank;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.capgemini.pecunia.bank.web.LoggerInterceptor;
 
 @SpringBootApplication
 public class PecuniaBankApplication {
@@ -9,6 +12,8 @@ public class PecuniaBankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PecuniaBankApplication.class, args);
 	}
-
 	
+	@Autowired
+	public LoggerInterceptor loggerInterceptor;
+
 }
